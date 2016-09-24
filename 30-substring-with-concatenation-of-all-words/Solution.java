ef21bcb7d19bb17f4ae84dc9216e4fe4dca00dff
Masruba Tasnim <masruba@gmail.com>
2016-09-23 19:43:22 -0700

    30.1

--- 30-substring-with-concatenation-of-all-words/Solution.java
+++ 30-substring-with-concatenation-of-all-words/Solution.java
@@ -1,3 +1,15 @@
+/*
+You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+For example, given:
+s: "barfoothefoobarman"
+words: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).
+
+*/
+
 public class Solution {
     boolean match(String s, int start, Map<String, Integer> dict , int wordSize, int numWords){
         Map<String, Integer> cnt = new HashMap<String, Integer>();
