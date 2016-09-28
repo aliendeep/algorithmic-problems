bce74bfa32d191435b438b3d5617d575c5bc6162
Masruba Tasnim <masruba@gmail.com>
2016-09-27 18:54:09 -0700

    Added Problem Statement

--- 216-combination-sum-iii/Solution.java
+++ 216-combination-sum-iii/Solution.java
@@ -1,3 +1,25 @@
+/*
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and 
+each combination should be a unique set of numbers.
+
+
+Example 1:
+
+Input: k = 3, n = 7
+
+Output:
+
+[[1,2,4]]
+
+Example 2:
+
+Input: k = 3, n = 9
+
+Output:
+
+[[1,2,6], [1,3,5], [2,3,4]]
+*/
+
 public class Solution {
     public void bktk(int k, int target, int prev_i, int sum, List<Integer> cur, List<List<Integer>> result ){
         //  k numbers
--- 40-combination-sum-ii/Solution.java
+++ 40-combination-sum-ii/Solution.java
@@ -1,3 +1,20 @@
+/*
+Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+Each number in C may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8, 
+A solution set is: 
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+*/
 public class Solution {
     public void bktk(int[] candidates, int target, int prev_i, int sum, List<Integer> cur, List<List<Integer>> result ){
         if(sum == target){
